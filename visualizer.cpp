2764ad8f9698afc88efe92fb770081bb2542fefd
datakur1 <90991863+dogukan33@users.noreply.github.com>
2023-04-13 18:42:25 +0300

    Update visualizer.cpp

--- visualizer.cpp
+++ visualizer.cpp
@@ -1,27 +1,102 @@
+#include <stdlib.h>
 #include <iostream>
+#include <sstream>
 
-#include "Node.h"
+#include "BST.h"
+
+using namespace std;
 
 void printMenu();
-int checkMenuInput(std::string userInput);
+
+bool isInteger(const string& str);
 
 int main(int argc, char const *argv[])
 {
+    //avoid "unused parameters warning"
+    (void) argc;
+    (void) argv;
+
+    BST* bst = new BST();
     while(true) 
     {
-        std::string userInput;
+        string userInput;
         printMenu();
-        std::cin>>userInput;
-        int menuInput = checkMenuInput(userInput);
+        cin>>userInput;
 
-        switch(menuInput) 
-        {   
-            // end the program
-            case -1:
-                break;
-            // create a bst (first time, or create new?)
-            case 0:
-                break;
+        if(userInput=="1") {
+
+            //checking the format of input
+            string valueInput;
+            int value;
+            cout<<"Enter a positive integer: ";
+            cin>>valueInput;
+            if(isInteger(valueInput)) {
+                stringstream(valueInput) >> value;
+            }
+            else {
+                cout<<"Insertion failed. Not an integer."<<endl;
+                continue;
+            }
+            //if it comes here, then it means input format was ok
+            cout<<"Before insertion (inorder): ";
+            bst->inorder(bst->root);
+            cout<<"\n";
+            int insertStatus = bst->insert(value);
+            if(insertStatus==-1) {
+                cout<<"Insertion failed. Not a positive integer."<<endl;
+                continue;
+            }
+            else if(insertStatus==1) {
+                cout<<"Insertion failed. Value already exist in BST."<<endl;
+                continue;
+            }
+            else {
+                cout<<"Insertion succesful! Below is the updated BST."<<endl;
+                cout<<"After insertion (inorder): ";
+                bst->inorder(bst->root);
+                cout<<"\n";
+                continue;
+            }
+        }
+
+        else if(userInput=="2") {}
+
+        else if(userInput=="3") {
+            string valueInput;
+            int value;
+            cout<<"Enter a positive integer to search: ";
+            cin>>valueInput;
+            if(isInteger(valueInput)) {
+                stringstream(valueInput) >> value;
+            }
+            else {
+                cout<<"Insertion failed. Not an integer."<<endl;
+                continue;
+            }
+        }
+
+        else if(userInput=="4") {
+            cout<<"Preorder: ";
+            bst->preorder(bst->root);
+            cout<<endl;
+
+            cout<<"Inorder: ";
+            bst->inorder(bst->root);
+            cout<<endl;
+    
+            cout<<"Postorder: ";
+            bst->postorder(bst->root);
+            cout<<endl;
+        }
+
+        else if(userInput=="6") {
+            cout<<"Program terminated."<<endl;
+            break;
+        }
+
+        else {
+            cout<<"Invalid menu input. Try again."<<endl;
+            continue;
         }
 
     }
@@ -29,12 +104,13 @@ int main(int argc, char const *argv[])
 }
 
 void printMenu() {
+    std::cout<<"--------------------------------"<<endl;
     std::cout << "Binary Search Tree Visualizer\n"
             << "================================\n"
             << "1. Insert a value\n"
             << "2. Remove a value\n"
             << "3. Search for a value\n"
-            << "4. Display the tree\n"
+            << "4. Get traversals\n"
             << "5. Clear the tree\n"
             << "6. Exit\n"
             << "--------------------------------\n"
@@ -42,12 +118,24 @@ void printMenu() {
 }
 
 /**
- * @brief Checks 
+ * @brief takes the user input and checks whether it is an integer or not.
  * 
- * @param userInput 
- * @return (int) the number of the option if it is in valid range, else -1.
+ * @param str 
+ * @return true 
+ * @return false 
  */
-int checkMenuInput(std::string userInput) {
+bool isInteger(const string& str) {
+    if (str.empty()) return false;
 
+    size_t startIndex = 0;
+    if (str[0] == '-') {
+        if (str.length() == 1) return false; // Check if the input is only a minus sign
+        startIndex = 1;
+    }
+
+    for (size_t i = startIndex; i < str.length(); i++) {
+        if (!isdigit(str[i])) return false;
+    }
+
+    return true;
 }
-
